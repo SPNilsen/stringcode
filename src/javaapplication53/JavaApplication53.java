628253d6b9d71486f2a44e121cde8851f8b57b8b
SPNilsen <fili.tine@gmail.com>
2015-10-30 12:40:12 -0500

    Recent Update

--- src/javaapplication53/JavaApplication53.java
+++ src/javaapplication53/JavaApplication53.java
@@ -6,6 +6,7 @@
 package javaapplication53;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,27 +19,39 @@ public class JavaApplication53 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+// TODO code application logic here
+
         List<String> strings = new ArrayList();
 
-                  String myString = null;
+        String myString = null;
         strings.add(myString);
-        
-        strings.add("There");
-        
 
-        
-        strings.add("are");
-        
+        for (int q = 0; q < 5000; q++) {
+            strings.add("Value");
+        }
 
+        strings.add(myString);
 
-        strings.add("four");
-              
-        
-        
+        for (int q = 0; q < 5000; q++) {
+            strings.add("Value");
+        }
+
+        long startTime = System.currentTimeMillis();
         Boolean result = doesListHaveGaps(strings);
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println(elapsedTime);
 
         System.out.println("There result :" + result);
+
+        long nextStartTime = System.currentTimeMillis();
+        Boolean nextResult = doesThisListHaveGaps(strings);
+        long nextEndTime = System.currentTimeMillis();
+        long nextElapsedTime = nextEndTime - nextStartTime;
+        System.out.println(nextElapsedTime);
+
+        System.out.println("There result :" + result);
+
     }
 
     /**
@@ -57,23 +70,21 @@ public class JavaApplication53 {
         for (int i = 0; i < modifierCodeList.size(); i++) {
             if (previousValue && modifierCodeList.get(i) == null) {
                 //if the previous pass was a null value and this value is a null value check the remainder of the List
-                
+
                 //Build SubString List
                 List<String> subStrings = new ArrayList();
-                for(int s = i; s < modifierCodeList.size(); s++){
+                for (int s = i; s < modifierCodeList.size(); s++) {
                     subStrings.add(modifierCodeList.get(s));
                 }
-                
+
                 //Check for only Null Values - since we know that the previous value was a String and the current value is null, then if all the rest are null, then return TRUE
-                for (int j = 0; j < subStrings.size(); j++){
-                    System.out.println("Is Null Check");
-                    if(subStrings.get(j) != null){
+                for (int j = 0; j < subStrings.size(); j++) {
+                    if (subStrings.get(j) != null) {
                         return Boolean.FALSE;
                     } else {
-                        System.out.println("Is Null");
                     }
                 }
-                
+
             } else {
                 //if this current value has been evaluated to be a valid String, set the 'previousValue' to TRUE; this allows the IF check to test both conditions
                 previousValue = Boolean.TRUE;
@@ -81,4 +92,35 @@ public class JavaApplication53 {
         }
         return Boolean.TRUE;
     }
+
+    public static Boolean doesThisListHaveGaps(List<String> modifierCodeList) {
+
+        int firstNonNull = 0;
+        int lastNonNull = 0;
+
+        for (int i = 0; i < modifierCodeList.size(); i++) {
+            if (modifierCodeList.get(i) == null) {
+                firstNonNull = i;
+            }
+        }
+
+        for (int i = modifierCodeList.size() - 1; i >= 0; --i) {
+            if (modifierCodeList.get(i) == null) {
+                lastNonNull = i;
+            }
+        }
+
+        ArrayList<String> subList = new ArrayList();
+        for (int j = firstNonNull; j <= lastNonNull; j++) {
+            subList.add(modifierCodeList.get(j));
+        }
+
+        if (subList.contains(null)) {
+            return Boolean.FALSE;
+        } else {
+            return Boolean.TRUE;
+        }
+
+    }
+
 }
